c5ff942100391f7116adbac6a4369e02e8d37aa4
mostafa0496 <145875929+mostafa0496@users.noreply.github.com>
2026-08-08 20:35:59 +0300

    Add files via upload

---
+++ create.js
@@ -0,0 +1,18 @@
+var bt = document.getElementById("bttn")
+var p = document.getElementById("nigger")
+var cuser = "admin"
+var cpass = "123456"
+
+bt.addEventListener("click", function () {
+    var iuser = document.getElementById("username").value()
+    var ipass = document.getElementById("password").value()
+
+    if (iuser === cuser && ipass === cpass) {
+        p.innerHTML = "Correct"
+        p.style.color = "green"
+        window.location = "login.html"
+    } else {
+        p.innerHTML = "incorrect"
+        p.style.color = "red"
+    }
+});
---
+++ login.js
@@ -0,0 +1,16 @@
+var bt = document.getElementById("bttn")
+var p = document.getElementById("nigger")
+bt.addEventListener("click",function(){
+    var ps = document.getElementById("password").value
+    var cps = document.getElementById("cpassword").value
+
+    if(ps===cps){
+        p.style.color="green"
+        p.innerHTML="correct"
+        window.location = "login.html"
+    }
+    else{
+        p.style.color="red"
+        p.innerHTML="Fuck off"
+    }
+})
